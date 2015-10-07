9e6e8a13d0d4a765a679704378ea513eb7ddf3d3
cdeng001 <cdeng001@ucr.edu>
2015-10-07 00:30:50 -0700

    change explore folder, adopt page

--- explore-map.php
+++ explore-map.php
@@ -29,27 +29,25 @@
     </head>
     
     <body onload='myInit()'>
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-        <script src="library/imageMapResizer.min.js"></script>
         <?php include('./library/nav-bar.html'); ?>
+        <script src="library/imageMapResizer.min.js"></script>
+
         
         <div>
             <div class='map-pull-left'>
-                <img src="./resources/images/map.bmp"  alt="map" usemap="#mythic-map" id="map-image">
+                <img src="resources/images/map.bmp"  alt="map" usemap="#mythic-map" id="map-image">
                 <canvas id='myCanvas'></canvas>
                 <map class="mythic-map-obj" name="mythic-map">
-                    <area shape="poly" coords="131,76,181,45,227,49,236,59,257,81,262,120,189,160,186,175,97,155,118,94" alt="bird-mtn" href="./explore/bird-mtn.php">
-                    <area shape="poly" coords="386,70,436,36,483,54,516,99,450,143,419,156,382,141,375,102,377,84" alt="ice-mtn" href="./explore/ice.php">
-                    <area shape="poly" coords="15,258,118,202,200,234,136,278,201,306,16,419" alt="farm" href="./explore/farm.php">
-                    <area shape="poly" coords="352,138,344,162,342,198,342,232,338,237,320,248,316,252,317,282,375,289,417,303,438,290,397,278,445,221,405,196,391,204,387,216,384,236,366,230,365,201,359,191,361,167" alt="swamp" href="./explore/swamp-tower.php">
-                    <area shape="poly" coords="522,199,592,162,660,187,668,258,644,283,599,286,570,271,527,251,507,223" alt="mtn-city" href="./explore/right-mtn.php">
-                    <area shape="poly" coords="588,353,578,363,575,374,557,389,544,397,537,390,528,401,529,409,521,416,599,445,652,400,640,396,644,384,639,378,633,386,608,377,601,362" alt="dojo" href="./explore/pagoda.php">
-                    <area shape="poly" coords="168,508,153,476,190,470,162,382,212,351,256,385,218,422,269,426,293,466,264,514,226,540" alt="low-land" href=".explore/low-river.php">
-                    <area shape="poly" coords="379,336,368,400,341,429,328,458,343,505,548,538,558,519,478,463,461,424,475,404,453,389,421,412,416,360,395,335" alt="plat" href="./explore/plateau.php">
-                    <area shape="poly" coords="567,6,679,9,679,153,584,134,639,115,590,94,618,73,544,47,578,19,567,16,530,27,528,8" alt="ocean" href="./explore/ocean.php">
+                    <area shape="poly" coords="131,76,181,45,227,49,236,59,257,81,262,120,189,160,186,175,97,155,118,94" alt="bird-mtn" href="explore/bird-mtn">
+                    <area shape="poly" coords="386,70,436,36,483,54,516,99,450,143,419,156,382,141,375,102,377,84" alt="ice-mtn" href="explore/ice-mtn">
+                    <area shape="poly" coords="15,258,118,202,200,234,136,278,201,306,16,419" alt="farm" href="explore/farm">
+                    <area shape="poly" coords="352,138,344,162,342,198,342,232,338,237,320,248,316,252,317,282,375,289,417,303,438,290,397,278,445,221,405,196,391,204,387,216,384,236,366,230,365,201,359,191,361,167" alt="swamp" href="explore/swamp">
+                    <area shape="poly" coords="522,199,592,162,660,187,668,258,644,283,599,286,570,271,527,251,507,223" alt="mtn-city" href="explore/mtn-city">
+                    <area shape="poly" coords="588,353,578,363,575,374,557,389,544,397,537,390,528,401,529,409,521,416,599,445,652,400,640,396,644,384,639,378,633,386,608,377,601,362" alt="dojo" href="explore/dojo">
+                    <area shape="poly" coords="168,508,153,476,190,470,162,382,212,351,256,385,218,422,269,426,293,466,264,514,226,540" alt="low-land" href=".explore/low-lands">
+                    <area shape="poly" coords="379,336,368,400,341,429,328,458,343,505,548,538,558,519,478,463,461,424,475,404,453,389,421,412,416,360,395,335" alt="plat" href="explore/plateau">
+                    <area shape="poly" coords="567,6,679,9,679,153,584,134,639,115,590,94,618,73,544,47,578,19,567,16,530,27,528,8" alt="ocean" href="explore/ocean">
                 </map>
             </div>
             <div class='map-pull-right'>
