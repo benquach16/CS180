ca4da88958b7f7ac09a4826fc73072e3269322ab
Ben Quach <benquach16@yahoo.com>
2015-10-12 17:26:24 -0700

    querying from db

--- petpage.php
+++ petpage.php
@@ -4,7 +4,16 @@
   <?php
 	 //pull information from database
 	 include './library/opendb.php';
-	 
-	 ?>
+	 $db_socket = initSocket();
+	 $user = $_SESSION['curr_user'];
+	 $query = "SELECT user FROM auth_list";
+	 $statement = $db_socket->prepare($query);
+  $statement->execute();
+  if($statement->rowCount() == 1)
+  {
+  
+  }
+  include './library/closedb.php';
+  ?>
 </body>
 </html>
