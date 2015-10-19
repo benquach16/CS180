fc31108b9229c789aee77e4c73f9e7705efa41f1
ThatZaZaBean <zazabean@live.com>
2015-10-19 01:29:20 -0700

    well it's an improvement

---
+++ includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "tony_t1");    // The database username. 
+define("PASSWORD", "asdf");    // The database password. 
+define("DATABASE", "user_data");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
--- petpage.php
+++ petpage.php
@@ -1,170 +1,106 @@
 <!doctype html>
-<html style="height: 100%;">  
-    <body>
-            <?php include('./library/nav-bar.html'); ?>
-    <?php
-        //pull information from database
-        include './library/opendb.php';
-        
-    ?>
-    </body>     
 
+<html style="height: 100%;">
 
-    <?php   // Connect to user db...apparently not needed and I wasted my time QQ
-     $db = mysql_connect("localhost","root","dankmemes"); 
-     if (!$db) {
-        die("Database connection failed miserably: " . mysql_error());
-     }
-     else {
-       // echo "We in the Database Database", "<br/>\n";
-     }
-     
-    $db_select = mysql_select_db("test",$db);
-     if (!$db_select) {
-        die("Database selection also failed miserably: " . mysql_error());
-     }
-     else {
-       // echo "Database Database", "<br/>\n";
-     }
-     
-     $result = mysql_query("SELECT * FROM auth_list", $db);
-     if (!$result) {
-     die("Database query failed: " . mysql_error());
-     }
-     else {
-       // echo "Database Database", "<br/>\n"; 
-        
-     } 
-    ?>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-    <!-- For reasons unknown to me, the php call was bitching
-         when put below the CSS area, so it's thrown up here 
-         and it mostly still works.                 #Swag -->
-    <div id="Welcome">
-            <!-- add html//js for account features-->
-            <!-- ability to display character (pet) avatar-->
-            <!-- perhaps display character inventory? how do we design this?-->
-            <!-- maybe we can store an inventory as a JSON object in mysql-->
+    <?php include('./library/nav-bar.html'); ?>          
+    <div id="Welcome">           
             <H1>Welcome back, <?php include('/library/getUser.php'); getUser(); ?>!</H1>
     </div>
 
-    <!-- CSS styles for div sections -->
-    <style>
-    #Youtube {
-        position:relative;
-        line-height:1px;
-        background-color:black;
-        height:281px;
-        width:493px;
-        top: 5%;
-        right: 5%;
-        float:none;
-        border-style:ridge;
-        border-color:red;
-        border-width:6px;
-        padding:0px;    
-    }
-    #Welcome {
-        top:0px;
-        background-color:black;
-        color:white;
-        text-align:center;
-        padding:5px;
-    }
-    #Sidebar {
-        line-height:50px;
-        position:absolute;        
-        background-color: hsla(352, 47%, 44%, .4);
-        float:left;
-        width:10%;
-        height:100%;
-        padding:20px;
-    }
-    #FakeButton {
-        position:relative;
-        border:2px solid black;
-        width:120px;
-        height:45px;
-        border-radius:7%;
-        background-color:hsla(10, 100%, 0%, .3);
-        color:white;
-        padding: 0px 25px 25px 25px;
-        
-    }
-    
-    #MetaButton {
-        position:relative;
-        border:2px solid black;
-        width:120px;
-        height:45px;
-        border-radius:7%;
-        background-color:hsla(10, 100%, 0%, .3);
-        color:white;
-        padding: 15px 25px 25px 25px;
-        
-    }
-    #Wall {
-        position:relative;
-        background-color: hsla(0, 0%, 50%, .5);
-        float:right;
-        border:2px solid black;
-        width:90%;
-        height:100%;        
-        padding:10px;
-    }    
-    </style>
+              
+    <?php //pull information from database 
+    include './library/opendb.php';
+    ?>
 
+ <!-- CSS styles for div sections -->
+     <style>
+        #Youtube {
+            position:relative;
+            line-height:1px;
+            background-color:black;
+            height:270px;
+            width:36%;
+            top: 0%;
+            right: 0%;
+            float:none;
+            padding:0px;    
+        }
+        #Welcome {
+            top:0px;
+            background-color:black;
+            color:white;
+            text-align:center;
+            padding:5px;
+        }
+        #Sidebar {
+            line-height:50px;
+            position:absolute;        
+            background-color: hsla(352, 47%, 44%, .4);
+            float:left;
+            width:10%;
+            height:100%;
+            padding:20px;
+        }
+        
+        #MetaButton {
+            position:relative;
+            width:120px;
+            height:45px;
+            background-color:hsla(10, 100%, 0%, .3);
+            color:white;
+            padding: 15px 25px 25px 25px;
+            
+        }
+        #Wall {
+            position:relative;
+            background-color: hsla(0, 0%, 50%, .5);
+            float:right;
+            width:85%;
+            height:100%;        
+            padding:0px;
+        }
+        .form-inline{
+            float:left;
+            padding-top:2%;
+            padding-bottom:2%;
+            width:64%;
+            background-image: url("http://cdn.myanimelist.net/images/anime/8/76479.jpg");
+        }
+    </style>
+    
     <head> <!-- Title of page -->
         <title>Pet Page</title>
     </head>
 
-    <body style="height: 100%;"> 
+    <body style="height: 100%;">
         <div id="Wall"> <!-- Facebooky part of page WIP-->
             <div id="Youtube" style="float:right; position:relavtive;">    
                     <body> <!-- Emmbedded youtube video...for style later -->
-                        <iframe width="480" height="270" src="http://www.youtube.com/embed/eW6sEkTGbUc" 
+                        <iframe width=100% height=100% src="http://www.youtube.com/embed/eW6sEkTGbUc" 
                          style="position:relative;"
                          >
                         </iframe>    
                     </body>   
-            </div> 
-        </div>
-        
-            <div id="Sidebar"> <!-- Didn't realize there was buttons, so here's some fake ones WIP-->
+            </div>
             
-                <div id="FakeButton">
-                    Friends
-                </div>
-                <br>
-                <div id="FakeButton">
-                    Pets
-                </div>
-                <br>
-                <div id="FakeButton">
-                    Costumes
-                </div>
-                <br>
-                <div id="FakeButton">
-                    Pictures
-                </div>
-                <br>
-                
-                <button id="MetaButton" style="background-color:lightred;" type="button" class="btn";
-                onClick="location.href='http://localhost/petpage/nothing_here.php'">Friends</button>
-                <button id="MetaButton" style="background-color:lightred;"  type="button" class="btn";
-                onClick="location.href='https://google.com'">Pets</button>
-                <button id="MetaButton" style="background-color:lightred;" type="button" class="btn";
-                onClick="location.href='https://facebook.com'">Costumes</button>
-                <button id="MetaButton" style="background-color:lightred;" type="button" class="btn";
-                onClick="location.href='https://en.wikipedia.org/wiki/G-Dragon'">Pictures</button>
-                
-            </div>     
+            <!-- Wall posts -->
+            <form class="form-inline" role="Post">
+                <div class="form-group_2">
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" placeholder="What are you feeling?">
+                    </div>
+                    <button type="button" class="btn btn-default col-md-2">Post!</button>
+                </div>                
+            </form>
+            
+        </div>
+
+    
+        <?php include('./library/vert-nav-bar.html'); ?>
     </body>
     
 </html>
-
-
-
-<?php   // Close DB connection
- mysql_close($db);
-?>
