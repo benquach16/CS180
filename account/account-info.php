0515e5728ff73326dd7ce9fe460db1fa5c323434
cdeng001 <cdeng001@ucr.edu>
2015-11-02 18:15:31 -0800

    small commit

--- account/account-info.php
+++ account/account-info.php
@@ -5,7 +5,7 @@
     <head>
         <title>Account Info</title>
     </head>
-    <body>
+    <body onload="onInit();">
         <style>
             .pet-party-display{
                 border: solid;
@@ -35,13 +35,58 @@
         </div>
 
         <div class="pet-party-display">
-            <div>
-                <?php include('get-pet.php'); petInfoSetup();?>
+            <div id="pet-section">
             </div>
         </div>
 
-        <script>
+        <script src="https://www.rootcdn.com/libs/pixi.js/3.0.7/pixi.min.js" ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
+        <script>
+            jQuery.extend({
+                getValues: function(url, method, data, type) {
+                    var result = null;
+                    $.ajax({
+                        url: url,
+                        type: method,
+                        data: data,
+                        dataType: type,
+                        //contentType: 'application/json',
+                        async: false,
+                        success: function(data) {
+                            result = jQuery.parseJSON(data);
+                        },
+                        error: function(xhr){
+                            result = jQuery.parseJSON(xhr.responseText);
+                        },
+                    });
+                    return result;
+                }
+            });
+
+            function populatePetSection(pet_ary){
+                var section = $('#pet-section');
+                for(var i = 0; i < pet_ary.pet_list.length; i++){
+                    var pet = document.createElement('DIV');
+                    pet.id = "pet"+i;
+
+                    var img = document.createElement('IMG');
+                    img.src = '../'+pet_ary.pet_list[i].base;
+                    img.height = 
+                    $(pet).append(img);
+
+                    var name = document.createElement('P');
+                    $(name).append(pet_ary.pet_list[i].name);
+                    $(pet).append(name);
+
+                    console.log(pet);
+                    section.append(pet);
+                }
+            }
+
+            function onInit(){
+                populatePetSection($.getValues("get-pet.php", "GET", "user_id=<?php echo $_SESSION['curr_id']; ?>", "application/x-www-form-urlencoded"));
+            }
         </script>
     </body>
 </html>
