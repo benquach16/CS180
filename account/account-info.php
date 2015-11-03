8dd1bbea4cba5bf3a4c0011698b950735e735c97
cdeng001 <cdeng001@ucr.edu>
2015-11-02 22:04:47 -0800

    new acct info

--- account/account-info.php
+++ account/account-info.php
@@ -64,27 +64,70 @@
                 }
             });
 
+            var renderers = [];
+            var containers = [];
+            var globalID;
+
             function populatePetSection(pet_ary){
                 var section = $('#pet-section');
                 for(var i = 0; i < pet_ary.pet_list.length; i++){
                     var pet = document.createElement('DIV');
+                    $(pet).attr('style', 'width: 250px; height: 270; border: solid 1px; border-color: lime;');
                     pet.id = "pet"+i;
 
-                    var img = document.createElement('IMG');
-                    img.src = '../'+pet_ary.pet_list[i].base;
-                    img.height = 
-                    $(pet).append(img);
+                    var canvas = document.createElement('CANVAS');
+                    canvas.id = 'canvas' + i;
+                    canvas.height = 250;
+                    canvas.width = 250;
+                    $(pet).append(canvas);
 
                     var name = document.createElement('P');
+                    $(name).attr('style', 'width: 250px; border: solid 1px; border-color: lime');
                     $(name).append(pet_ary.pet_list[i].name);
                     $(pet).append(name);
 
                     section.append(pet);
+
+                    var container = new PIXI.Container(0x66FF99);
+                    containers.push (container);
+
+                    var texture = PIXI.Texture.fromImage("../"+pet_ary.pet_list[i].base);
+                    // create a new Sprite using the texture
+                    var img = new PIXI.Sprite(texture);
+
+                    // center the sprites anchor point
+                    img.anchor.x = 0.5;
+                    img.anchor.y = 0.5;
+                    // move the sprite t the center of the screen
+                    img.position.x = canvas.width / 2;
+                    img.position.y = canvas.height / 2;
+                    container.addChild(img);
                 }
+            }
+
+            function setupRenderers(){
+                $('#pet-section').children().each(function(i){
+                    var c = document.getElementById('canvas'+i);
+                    renderers.push(new PIXI.autoDetectRenderer(c.width, c.height, {view: c}) );
+                });
+            }
+
+            function animate(timestamp){
+                for(var i = 0; i < renderers.length; i++){
+                    renderers[i].render(containers[i]);
+                }
+
+                globalID = requestAnimationFrame( function(timestamp){
+                    animate(timestamp);
+                } );
             }
 
             function onInit(){
                 populatePetSection($.getValues("get-pet.php", "GET", "user_id=<?php echo $_SESSION['curr_id']; ?>", "application/x-www-form-urlencoded"));
+                setupRenderers();
+                globalID = requestAnimationFrame( function(timestamp){
+                    animate(timestamp);
+                } );
             }
         </script>
     </body>
