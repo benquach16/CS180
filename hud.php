8fb60cdee1758287ac6d708d4fac36739706e734
Ben Quach <benquach16@yahoo.com>
2015-11-07 20:18:32 -0800

    setup homepage

--- hud.php
+++ hud.php
@@ -13,35 +13,63 @@
         </script>
 		<div class = "container center-text">
 
-
-			<div class = "panel panel-default">
-				<div class="panel-heading">
-					<h3 class = "panel-title">Homepage</h3>
-				</div>
-
-				<div class ="panel-body">
-					<ul class="list-group" id="listOfPosts">
-						<li class="list-group-item">
-						<!-- put a specific modal for each post here -->
-						<button type="button" class="btn btn-default btn-lg">
-						  <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Star
-						</button>
-	
-						</li>
-					</ul>
-				</div>
-			</div>		
+		<div class="jumbotron">
+			<h1>Latest News!</h1>
+			<p>Check out our latest patch note and features!</p>
+			<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 		</div>
+		<div class = "panel panel-default">
+			<div class="panel-heading">
+				<h3 class = "panel-title">Homepage</h3>
+			</div>
+			<div class ="panel-body">
+				<ul class="list-group" id="listOfPosts">
+	
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Panel title</h3>
+						</div>
+						<div class="panel-body">
+							Panel content
+						</div>
+					</div>
+	
+				</ul>
+			</div>
+		</div>		
+	</div>
 		
 		
     </body>
 	<script>
-		var listOfPosts = document.getElementById("listOfPost");
-		for(var i = 0; i < 5;i++)
-		{
-			document.createElement('ul');
-						
-		}
+		$.ajax({
+			url:'library/getFriendsPosts.php',
+			complete: function (response)
+			{
+				var arr = JSON.parse(response.responseText);
+				var listOfPosts = document.getElementById("listOfPosts");
+				for(var i = 0; i < 5;i++)
+				{
+					//this looks shitty
+					var newPost = document.createElement('div');
+					newPost.className = "panel panel-primary";
+					var heading = document.createElement('div');
+					heading.className = "panel-heading";
+					var textHeader = document.createTextNode("header");
+					heading.appendChild(textHeader);
+
+					var body = document.createElement('div');
+					body.className = "panel-body";
+					var textBody = document.createTextNode("body");
+					body.appendChild(textBody);
+			
+					newPost.appendChild(heading);
+					newPost.appendChild(body);
+					listOfPosts.appendChild(newPost);
+				}
+			}
+		});
+
 	</script>
 	<div id="chatbar">
       <?php include('./library/chat-bar.html'); ?>
