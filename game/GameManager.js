e5ee1a9ef1fd4d6913c7b8a1b6d16588dddc6b21
dylanjay <dylanjay9@gmail.com>
2015-12-03 20:21:12 -0800

    reset function added

--- game/GameManager.js
+++ game/GameManager.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(840, 600, Phaser.AUTO, '', { preload: preload, create
 var grid;
 var playerLeft;
 var playerRight;
-var input;
+var gameOver;
 
 var ConnData = {
 	Null: "Null",
@@ -90,10 +90,37 @@ function preload() {
   
 }
 
+function reset() 
+{	
+	if(gameOver)
+	{
+		playerLeft.reset();
+		playerRight.reset();
+	}
+	
+	var weaponsLeft = [];
+	weaponsLeft[0] = new Weapon(10, 1000, 250, TileType.Red);
+	weaponsLeft[1] = new Weapon(20, 500, 1000, TileType.Red);
+	var weaponsRight = [];
+	weaponsRight[0] = new Weapon(10, 1000, 250, TileType.Blue);
+	weaponsRight[1] = new Weapon(20, 500, 1000, TileType.Blue);
+    playerLeft = new Player(0,1, 'gatorLeft', weaponsLeft);
+	playerRight = new Player(5, 1, 'gatorRight', weaponsRight);
+	projectileGroup = game.add.group();
+	projectileGroup.enableBody = true;
+	projectileGroup.allowGravity = false;
+	healthLeft = new Bar(50, 50, 300, 20, 0xff0000);
+	healthRight = new Bar(450, 50, 300, 20, 0x0000ff);
+	
+	gameOver = false;
+}
+
 function create() {
 
-	grid = new Grid(70, 450, 800, 600, 6, 3);
+	gameOver = false;
 
+	grid = new Grid(70, 450, 800, 600, 6, 3);
+	
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -107,19 +134,7 @@ function create() {
 	
 	grid.createGrid();
 	
-	var weaponsLeft = [];
-	weaponsLeft[0] = new Weapon(10, 1000, 250, TileType.Red);
-	weaponsLeft[1] = new Weapon(20, 500, 1000, TileType.Red);
-	var weaponsRight = [];
-	weaponsRight[0] = new Weapon(10, 1000, 250, TileType.Blue);
-	weaponsRight[1] = new Weapon(20, 500, 1000, TileType.Blue);
-    playerLeft = new Player(1,1, 'gatorLeft', weaponsLeft);
-	playerRight = new Player(4, 1, 'gatorRight', weaponsRight);
-	projectileGroup = game.add.group();
-	projectileGroup.enableBody = true;
-	projectileGroup.allowGravity = false;
-	healthLeft = new Bar(50, 50, 300, 20, 0xff0000);
-	healthRight = new Bar(450, 50, 300, 20, 0x0000ff);
+	reset();
 }
 
 function update() {
@@ -129,6 +144,11 @@ function update() {
 	playerRight.update();
 	
 	grid.tileUpdate();
+	if(gameOver)
+	{
+		playerLeft.disableInput();
+	}
+	
 	for(var i = 0; i < projectiles.length; i++)
 	{
 		if(projectiles[i].isFinished(i)){continue;}
